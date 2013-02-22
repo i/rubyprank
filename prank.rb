8fe0f4363f4b31afb6f636209905b25e612e137b
Ian Lozinski <ian.lozinski@gmail.com>
2013-02-22 14:37:45 -0500

    making progress

--- prank.rb
+++ prank.rb
@@ -1,16 +1,22 @@
+require 'erubis'
 require 'sinatra'
 require 'haml'
 require 'oauth'
 require 'twilio-ruby'
+require 'uuid'
 
 twiliosid  = "AC566da319c49345fe4fbbbea81ada1de0"
 twilioauth = "741080b04bcb6c2471cf9439d939abce"
+appsid = 'APb99a0ecfdf3a3a1e3fbcc7359c72d9a0'
 
 $client = Twilio::REST::Client.new twiliosid, twilioauth
+$capability = Twilio::Util::Capability.new twiliosid, twilioauth
+$capability.allow_client_outgoing appsid
 
+$token = $capability.generate
 
 get '/phone' do
-    File.open('./views/browser-phone/browser-phone.php').read
+    haml :calling, :locals => {:token => $token}
 end
 
 get '/' do
@@ -23,20 +29,28 @@ end
 
 post '/sms' do
     number = params['number']
-    message = $client.account.sms.messages.create "Jenny please?! I love you <4", params['number'], "+14159686840"
+    message = $client.account.sms.messages.create "Jenny please?! I love you <4", params['number'], "4159686840"
 end
 
 post '/call' do
-    #call1 = $client.account.calls.make "+14159686840", params['number1'], "http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient"
-    #call2 = $client.account.calls.make "+14159686840", params['number2'], "http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient"
-    #call = $client.account.calls.make "+14159686840", params['number1'], "http://twimlets.com/conference?Moderators%5B0%5D=6093067701&Moderators%5B1%5D=2019608602&"
+    uuid = UUID.generate
     haml :calling, :locals => {:number1 => params['number1'], :number2 => params['number2']}
-    call1 = $client.account.calls.make "+14159686840", params['number1'], "http://twimlets.com/conference?Name=foo"
-    call2 = $client.account.calls.make "+14159686840", params['number2'], "http://twimlets.com/conference?Name=foo"
-
-    #call = $client.account.calls.make "+14159686840", params['number1'], "http://twimlets.com/conference?Moderators%5B0%5D=6093067701&Moderators%5B1%5D=2019608602&"
+    call1 = $client.account.calls.make "+14159686840", params['number1'], "http://twimlets.com/conference?Name=#{uuid}&Message=%20"
+    call2 = $client.account.calls.make "+14159686840", params['number2'], "http://twimlets.com/conference?Name=#{uuid}&Message=%20"
 end
+
 
 get '/fad' do
     @client.class.to_s
 end
+
+get '/monkey' do
+    # Find these values at twilio.com/user/account
+    # This application sid will play a Welcome Message.
+    demo_app_sid = 'APabe7650f654fc34655fc81ae71caa3ff'
+    capability = Twilio::Util::Capability.new twiliosid, twilioauth
+    capability.allow_client_outgoing demo_app_sid
+    token = capability.generate
+    erb :dex, :locals => {:token => token}
+end
+
